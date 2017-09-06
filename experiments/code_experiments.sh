27b1bb8e33ebfee54789974be4eff6902c9a2670
Kyle Richardson <kyle@ims.uni-stuttgart.de>
2017-09-06 14:07:24 +0200

    added the experiment directory

---
+++ experiments/code_experiments.sh
@@ -0,0 +1,30 @@
+# This script tells you how to get the data and associated pipelines for reproducing the following two
+# publications: 
+
+
+# @inproceedings{richardson-kuhn:2017:Long,
+#   author    = {Richardson, Kyle  and  Kuhn, Jonas},
+#   title     = {Learning {S}emantic {C}orrespondences in {T}echnical {D}ocumentation},
+#   booktitle = {Proceedings of the ACL},
+#   year      = {2017},
+#   url={http://aclweb.org/anthology/P/P17/P17-1148.pdf},
+#   }
+
+# @inproceedings{richardson-kuhn:2017:Demo,
+#   author    = {Richardson, Kyle  and  Kuhn, Jonas},
+#   title     = {Function {A}ssistant: {A} {T}ool for {NL} {Q}uerying of {API}s},
+#   booktitle = {Proceedings of the EMNLP},
+#   year      = {2017},
+#   }
+
+# If you have any problems reproducing the results, or setting up the infrastructure, please
+# write an email to kyle@ims.uni-stuttgart.de
+
+
+## best to call from ZUBR source directory
+
+
+### GETTTING THE CODE DATA: the data is hosted here for both datasets: https://github.com/yakazimir/Code-Datasets
+
+
+## download the pipeline and associated scripts
